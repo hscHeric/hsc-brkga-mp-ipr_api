32aed9c9c630a10efd55e67a68646d5380e60b04
Heric da Silva Cruz <hericsilvaho@gmail.com>
2026-05-06 09:20:24 -0300

    Organize BRKGA-MP-IPR as CMake library

--- test/module_a.cpp
+++
@@ -1,7 +0,0 @@
-#include "brkga_mp_ipr.hpp"
-
-int main() {
-    auto params = BRKGA::readConfiguration("config.conf");
-    return 0;
-}
-
--- test/module_b.cpp
+++
@@ -1,6 +0,0 @@
-#include "brkga_mp_ipr.hpp"
-
-void test() {
-    auto params = BRKGA::readConfiguration("config.conf");
-}
-
